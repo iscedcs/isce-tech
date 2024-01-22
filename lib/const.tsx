fbc5d793fd347e41083b58b0630b1049e4c9cd57
LeoDivine <divineonyi2004@gmail.com>
2024-01-22 20:05:58 +0100

    Latest commit

--- lib/const.tsx
+++ lib/const.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   Customer,
   Data,
@@ -7,9 +6,6 @@ import {
   Innovation,
   Result,
 } from "./svg";
-=======
-import { UserPlusIcon, HeartPulseIcon, LightbulbIcon } from "lucide-react";
->>>>>>> 053c69c4082a5f674e578bc71cf661a27a3ebc3f
 
 export const PROJECT_ITEM: IPROJECT[] = [
   {
@@ -39,27 +35,31 @@ export const PROJECT_ITEM: IPROJECT[] = [
     figlink: "",
   },
 ];
-<<<<<<< HEAD
 export const services: IService[] = [
   {
-    title: 'Tech Solutions',
+    title: "Tech Solutions",
     description: `Harness the full potential of technology with ISCE's comprehensive Tech Solutions. From optimizing business processes to enhancing personal experiences, we offer a range of innovative solutions tailored to meet your unique needs. Unlock efficiency, connectivity, and seamless integration with our cutting-edge tech solutions.`,
   },
   {
-    title: 'Tech Consultancy',
-    description: `Leverage the expertise of ISCE's Tech Consultancy services to navigate the rapidly evolving tech landscape. Our seasoned consultants provide strategic insights, helping you make informed decisions and stay ahead in the dynamic world of technology. From planning to implementation, we guide you towards technological excellence.`,  },
+    title: "Tech Consultancy",
+    description: `Leverage the expertise of ISCE's Tech Consultancy services to navigate the rapidly evolving tech landscape. Our seasoned consultants provide strategic insights, helping you make informed decisions and stay ahead in the dynamic world of technology. From planning to implementation, we guide you towards technological excellence.`,
+  },
   {
-    title: 'Software Development',
-    description:`Empower your business with bespoke software solutions crafted by ISCE's expert developers. Our Software Development services cover a spectrum of needs, from user-friendly applications to robust backend systems. We bring your ideas to life with agile development processes and a commitment to delivering high-quality software.`  },
+    title: "Software Development",
+    description: `Empower your business with bespoke software solutions crafted by ISCE's expert developers. Our Software Development services cover a spectrum of needs, from user-friendly applications to robust backend systems. We bring your ideas to life with agile development processes and a commitment to delivering high-quality software.`,
+  },
   {
-    title: 'Hardware',
-    description:`Seamlessly integrate hardware solutions into your tech ecosystem with ISCE's Hardware Integrations service. Whether it's connecting devices for improved functionality or optimizing hardware performance, we ensure that your hardware components work harmoniously to enhance overall efficiency and productivity.`  },
+    title: "Hardware",
+    description: `Seamlessly integrate hardware solutions into your tech ecosystem with ISCE's Hardware Integrations service. Whether it's connecting devices for improved functionality or optimizing hardware performance, we ensure that your hardware components work harmoniously to enhance overall efficiency and productivity.`,
+  },
   {
-    title: 'NFC ',
-    description:`Explore the potential of contactless living with ISCE's NFC solutions. From secure mobile payments to smart interactions, our NFC technology services redefine convenience. Embrace the future with secure, efficient, and innovative NFC solutions that simplify daily interactions and transactions.`  },
+    title: "NFC ",
+    description: `Explore the potential of contactless living with ISCE's NFC solutions. From secure mobile payments to smart interactions, our NFC technology services redefine convenience. Embrace the future with secure, efficient, and innovative NFC solutions that simplify daily interactions and transactions.`,
+  },
   {
-    title: 'Smart Works',
-    description:`Transform your workspace into a smart, connected environment with ISCE's Smart Works solutions. Enhance efficiency, optimize workflows, and create a connected workplace that adapts to your needs. Our Smart Works services encompass smart office solutions, IoT integrations, and advanced technologies to elevate your work environment.`  },
+    title: "Smart Works",
+    description: `Transform your workspace into a smart, connected environment with ISCE's Smart Works solutions. Enhance efficiency, optimize workflows, and create a connected workplace that adapts to your needs. Our Smart Works services encompass smart office solutions, IoT integrations, and advanced technologies to elevate your work environment.`,
+  },
 ];
 
 export const CORE_VALUES: ICOREVALUE[] = [
@@ -98,25 +98,5 @@ export const CORE_VALUES: ICOREVALUE[] = [
     title: "Result Driven",
     description:
       "Focusing on delivering tangible results and measurable impact, aligning efforts with organizational goals and objectives.",
-=======
-export const coreValues: CoreValues[] = [
-  {
-    icon: <UserPlusIcon className="text-[#bd1e59] text-4xl mb-4" />,
-    title: "Effortless Connectivity",
-    description:
-      "ISCE harnesses the power of NFC technology to create an ecosystem where devices seamlessly communicate with a simple tap. Enjoy the convenience of effortless connectivity, making daily tasks smoother and more efficient.",
-  },
-  {
-    icon: <HeartPulseIcon className="text-[#bd1e59] text-4xl mb-4" />,
-    title: "Contactless Solutions for All",
-    description:
-      "ISCE is dedicated to bringing the benefits of contactless technology to everyone. Whether its secure payments access control or smart interactions ISCEs solutions cater to diverse needs, ensuring a user-friendly experience for individuals and businesses alike",
-  },
-  {
-    icon: <LightbulbIcon className="text-[#bd1e59] text-4xl mb-4" />,
-    title: "Innovative Applications",
-    description:
-      " ISCE goes beyond the ordinary by integrating NFC into innovative applications that redefine daily routines. From smart homes to connected workplaces, ISCE's solutions open doors to a world where technology enhances, rather than complicates, our lives ",
->>>>>>> 053c69c4082a5f674e578bc71cf661a27a3ebc3f
   },
 ];
