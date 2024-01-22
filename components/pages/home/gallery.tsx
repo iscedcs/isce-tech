14f84b08fd6cec9a66c49b8cc94b59d60c0f64c5
LeoDivine <divineonyi2004@gmail.com>
2024-01-22 14:43:59 +0100

    Latest Commit

--- components/pages/home/gallery.tsx
+++ components/pages/home/gallery.tsx
@@ -2,90 +2,21 @@ import { Button } from '@/components/ui/button';
 import MaxWidthContainer from '@/components/ui/container';
 import Image from 'next/image';
 
+import Projectitem from "./projectitem";
+
 export default function Gallery() {
-	return (
-		<div
-			className='bg-primary relative'
-			style={{
-				backgroundImage: `url('/teamSection.jpg')`,
-				backgroundSize: 'cover',
-				backgroundPosition: 'center',
-			}}
-		>
-			<div className='absolute bg-foreground/30 w-full h-full top-0 left-0'></div>
-			<MaxWidthContainer className='relative'>
-				<h2 className='text-background text-5xl font-bold'>{`GALLERY/WORKDONE`}</h2>
-				<p className='text-background mt-4 max-w-xl text-lg'>
-					{`Feature your most important posts in a carousel that's designed to show posts from different platforms and
-      links seamlessly side by side.`}
-				</p>
-				<div className='flex justify-center mt-8 space-x-4'>
-					<div className='bg-secondary rounded-lg overflow-hidden shadow-lg'>
-						<Image
-							alt='Interior'
-							className='w-full h-auto'
-							height={200}
-							src='/imagetest2.jpg'
-							style={{
-								aspectRatio: '300/200',
-								objectFit: 'cover',
-							}}
-							width={300}
-						/>
-						<div className='p-4'>
-							<Button className='bg-primary text-secondary'>{`Check out my new video!`}</Button>
-						</div>
-					</div>
-					<div className='bg-secondary rounded-lg overflow-hidden shadow-lg'>
-						<Image
-							alt="Isabella O'Connor"
-							className='w-full h-auto'
-							height={200}
-							src='/imagetest3.jpg'
-							style={{
-								aspectRatio: '300/200',
-								objectFit: 'cover',
-							}}
-							width={300}
-						/>
-						<div className='p-4'>
-							<p className='text-lg font-semibold'>{`Isabella O'Connor`}</p>
-						</div>
-					</div>
-					<div className='bg-secondary rounded-lg overflow-hidden shadow-lg'>
-						<Image
-							alt='Interior'
-							className='w-full h-auto'
-							height={200}
-							src='/imagetest2.jpg'
-							style={{
-								aspectRatio: '300/200',
-								objectFit: 'cover',
-							}}
-							width={300}
-						/>
-						<div className='p-4'>
-							<Button className='bg-primary text-secondary'>{`Interior trends 2023`}</Button>
-						</div>
-					</div>
-					<div className='bg-secondary rounded-lg overflow-hidden shadow-lg'>
-						<Image
-							alt='Rose'
-							className='w-full h-auto'
-							height={200}
-							src='/imagetest1.jpg'
-							style={{
-								aspectRatio: '300/200',
-								objectFit: 'cover',
-							}}
-							width={300}
-						/>
-						<div className='p-4'>
-							<Button className='bg-primary text-secondary'>{`Check out my new video!`}</Button>
-						</div>
-					</div>
-				</div>
-			</MaxWidthContainer>
-		</div>
-	);
+  return (
+    <div>
+      <div className="absolute bg-foreground/30 w-full h-full top-0 left-0"></div>
+      <MaxWidthContainer className="relative">
+        <h2 className="text-background text-center text-5xl font-bold">{`Our Projects`}</h2>
+        <p className="text-background mt-4 text-center text-lg">
+          {`We ensure client satisfaction with our timely and creative projects `}
+        </p>
+        <div className="text-white">
+          <Projectitem />
+        </div>
+      </MaxWidthContainer>
+    </div>
+  );
 }
