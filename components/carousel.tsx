74678e07661bcd73104963ab8ef323ab23e8d19c
Jeff AliG <aligwekwe2000j@gmail.com>
2024-01-22 10:26:09 +0100

    update of the header

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,87 +1,73 @@
-'use client';
+"use client";
 import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-} from '@/components/ui/carousel';
-import Autoplay from 'embla-carousel-autoplay';
-import HeaderCarouselDisplay from './carouseldisplay';
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import HeaderCarouselDisplay from "./carouseldisplay";
 
 export function HeaderCarouselComp() {
-	return (
-		<Carousel
-			opts={{
-				align: 'start',
-			}}
-			plugins={[
-				Autoplay({
-					delay: 5000,
-				}),
-			]}
-			className='w-full'
-		>
-			<CarouselContent>
-				<CarouselItem>
-					<HeaderCarouselDisplay
-						header='ISCE Tech'
-						content='ISCE is a tech company that primarily leverages in NFC technology to
-          simplify daily living by contactless solutions to anyone.'
-						link=''
-						className="bg-[url('/images/first.jpg')]"
-						buttonTitle='Call to action'
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<HeaderCarouselDisplay
-						header='A header'
-						content='ISCE is a tech company that primarily leverages in NFC technology to
-          simplify daily living by contactless solutions to anyone.'
-						link=''
-						className="bg-[url('/images/second.jpg')]"
-						buttonTitle='Call to action'
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<HeaderCarouselDisplay
-						header='A header'
-						content='ISCE is a tech company that primarily leverages in NFC technology to
-          simplify daily living by contactless solutions to anyone.'
-						link=''
-						className="bg-[url('/images/third.jpg')]"
-						buttonTitle='Call to action'
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<HeaderCarouselDisplay
-						header='ISCE Tech'
-						content='ISCE is a tech company that primarily leverages in NFC technology to
-          simplify daily living by contactless solutions to anyone.'
-						link=''
-						className="bg-[url('/images/fourth.jpg')]"
-						buttonTitle='Call to action'
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<HeaderCarouselDisplay
-						header='ISCE Tech'
-						content='ISCE is a tech company that primarily leverages in NFC technology to
-          simplify daily living by contactless solutions to anyone.'
-						link=''
-						className="bg-[url('/images/fifth.jpg')]"
-						buttonTitle='Call to action'
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<HeaderCarouselDisplay
-						header='ISCE Tech'
-						content='ISCE is a tech company that primarily leverages in NFC technology to
-          simplify daily living by contactless solutions to anyone.'
-						link=''
-						className="bg-[url('/images/sixth.jpg')]"
-						buttonTitle='Call to action'
-					/>
-				</CarouselItem>
-			</CarouselContent>
-		</Carousel>
-	);
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      plugins={[
+        Autoplay({
+          delay: 5000,
+        }),
+      ]}
+      className="w-full"
+    >
+      <CarouselContent>
+        <CarouselItem>
+          <HeaderCarouselDisplay
+            header="ISCE Tech"
+            content="We are providing world changing technology to empower your world."
+            link=""
+            className="bg-[url('/images/first.jpg')]"
+            buttonTitle="Call to action"
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <HeaderCarouselDisplay
+            header="ISCE Tech"
+            content="Be bold, unique and different; individuality is what makes us."
+            link=""
+            className="bg-[url('/images/second.jpg')]"
+            buttonTitle="Call to action"
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <HeaderCarouselDisplay
+            header="ISCE Tech"
+            content="ISCE is a tech company that primarily leverages in NFC technology to
+          simplify daily living by contactless solutions to anyone."
+            link=""
+            className="bg-[url('/images/fourth.jpg')]"
+            buttonTitle="Call to action"
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <HeaderCarouselDisplay
+            header="ISCE Tech"
+            content="We are a culture that inspires growth, development and connections, connection with yourself and the globe."
+            link=""
+            className="bg-[url('/images/fifth.jpg')]"
+            buttonTitle="Call to action"
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <HeaderCarouselDisplay
+            header="ISCE Tech"
+            content="Technology is limitless and we are bringing it to you."
+            link=""
+            className="bg-[url('/images/sixth.jpg')]"
+            buttonTitle="Call to action"
+          />
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  );
 }
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,120 +1,113 @@
-import React from 'react';
-import { Button } from '../ui/button';
-import { Separator } from '../ui/separator';
+import React from "react";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 import {
-	ArrowRightIcon,
-	Github,
-	Linkedin,
-	Twitter,
-	Youtube,
-} from 'lucide-react';
-import MaxWidthContainer from '../ui/container';
+  ArrowRightIcon,
+  Github,
+  Linkedin,
+  Twitter,
+  Youtube,
+} from "lucide-react";
+import MaxWidthContainer from "../ui/container";
 
 export default function Footer() {
-	return (
-		<div className='bg-background'>
-			<MaxWidthContainer className=' grid gap-10'>
-				<div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5  '>
-					<div className='grid gap-3 '>
-						<div className='text-lg font-bold '>Product</div>
-						<div className='grid gap-3'>
-							<div className=''>App</div>
-							<div className=''>Data</div>
-							<div className=''>Action </div>
-							<div className=''>Glide AI</div>
-							<div className=''>Integrations </div>
-						</div>
-						<Button
-							variant='link'
-							className=' justify-start px-0 font-bold text-accent-foreground gap-2'
-						>
-							{' '}
-							Contact sales{' '}
-							<ArrowRightIcon className='h-4 w-4' />
-						</Button>
-					</div>
-					<div className='grid gap-3'>
-						<div className='text-lg font-bold'>
-							{' '}
-							Solutions
-						</div>
-						<div className='grid gap-3'>
-							<div className=''>Business</div>
-							<div className=''>Operation </div>
-							<div className=''>IT&Engineering</div>
-							<div className=''> Agenies </div>
-							<div className=''>Field Teams</div>
-						</div>
-					</div>
-					<div className='grid gap-3'>
-						<div className='text-lg font-bold'>Use Cases</div>
-						<div className='grid gap-3'>
-							<div className=''>Work order Management</div>
-							<div className=''>Inspection Tools </div>
-							<div className=''>Inventroy Management</div>
-							<div className=''> Field Sales</div>
-							<div className=''> Knowledge Management</div>
-						</div>
-					</div>
-					<div className='grid gap-3'>
-						<div className='text-lg font-bold'>Connect</div>
-						<div className='grid gap-3'>
-							<div className=''>Google Sheets</div>
-							<div className=''> Airtable</div>
-							<div className=''>Big Table </div>
-							<div className=''>Excel </div>
-							<div className=''> BigQuery</div>
-						</div>
-					</div>
-					<div className='grid gap-3'>
-						<div className='text-lg font-bold'>
-							Resources{' '}
-						</div>
-						<div className='grid gap-3'>
-							<div className=''>Customer Stories</div>
-							<div className=''>App</div>
-							<div className=''>App</div>
-							<div className=''>App</div>
-							<div className=''>App</div>
-						</div>
-					</div>
-					<div className='grid gap-3 '>
-						<div className='text-lg font-bold'>Company</div>
-						<div className=' grid gap-3'>
-							<div className=''>App</div>
-							<div className=''>App</div>
-							<div className=''>App</div>
-							<div className=''>App</div>
-							<div className=''>App</div>
-						</div>
-					</div>
-				</div>
+  return (
+    <div className="bg-background">
+      <MaxWidthContainer className=" grid gap-10">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5  ">
+          <div className="grid gap-3 ">
+            <div className="text-lg font-bold ">Product</div>
+            <div className="grid gap-3">
+              <div className="">App</div>
+              <div className="">Data</div>
+              <div className="">Action </div>
+              <div className="">Glide AI</div>
+              <div className="">Integrations </div>
+            </div>
+            <Button
+              variant="link"
+              className=" justify-start px-0 font-bold text-accent-foreground gap-2"
+            >
+              Contact sales
+              <ArrowRightIcon className="h-4 w-4" />
+            </Button>
+          </div>
+          <div className="grid gap-3">
+            <div className="text-lg font-bold"> Solutions</div>
+            <div className="grid gap-3">
+              <div className="">Business</div>
+              <div className="">Operation </div>
+              <div className="">IT&Engineering</div>
+              <div className=""> Agenies </div>
+              <div className="">Field Teams</div>
+            </div>
+          </div>
+          <div className="grid gap-3">
+            <div className="text-lg font-bold">Use Cases</div>
+            <div className="grid gap-3">
+              <div className="">Work order Management</div>
+              <div className="">Inspection Tools </div>
+              <div className="">Inventroy Management</div>
+              <div className=""> Field Sales</div>
+              <div className=""> Knowledge Management</div>
+            </div>
+          </div>
+          <div className="grid gap-3">
+            <div className="text-lg font-bold">Connect</div>
+            <div className="grid gap-3">
+              <div className="">Google Sheets</div>
+              <div className=""> Airtable</div>
+              <div className="">Big Table </div>
+              <div className="">Excel </div>
+              <div className=""> BigQuery</div>
+            </div>
+          </div>
+          <div className="grid gap-3">
+            <div className="text-lg font-bold">Resources </div>
+            <div className="grid gap-3">
+              <div className="">Customer Stories</div>
+              <div className="">App</div>
+              <div className="">App</div>
+              <div className="">App</div>
+              <div className="">App</div>
+            </div>
+          </div>
+          <div className="grid gap-3 ">
+            <div className="text-lg font-bold">Company</div>
+            <div className=" grid gap-3">
+              <div className="">App</div>
+              <div className="">App</div>
+              <div className="">App</div>
+              <div className="">App</div>
+              <div className="">App</div>
+            </div>
+          </div>
+        </div>
 
-				<Separator />
-				<div className='flex flex-col md:flex-row justify-between gap-10  '>
-					<div className='max-w-96'>
-						{`ISCE's mission is to put the power, beauty, and magic of software
+        <Separator />
+        <div className="flex flex-col md:flex-row justify-between gap-10  ">
+          <div className="max-w-96">
+            {`ISCE's mission is to put the power, beauty, and magic of software
             development into the hands of a billion new creators. `}
-						<span className='text-blue-500'>Join Us</span>
-					</div>
-					<div className='flex flex-col lg:flex-row gap-8'>
-						<div className='flex flex-wrap gap-5'>
-							<div className=''>Status</div>
-							<div className=''>Terms</div>
-							<div className=''>Privacy</div>
-							<div className=''>OSS</div>
-							<div className=''>Sitemap</div>
-							<div className=''> Contact us</div>
-						</div>
-						<div className=''>
-							<div className='flex gap-5'>
-								<Youtube /> <Twitter /> <Linkedin />{' '}
-								<Github />
-							</div>
-						</div>
-					</div>
-				</div>
-			</MaxWidthContainer>
-		</div>
-	);
+            <span className="text-blue-500">Join Us</span>
+          </div>
+          <div className="flex flex-col lg:flex-row gap-8">
+            <div className="flex flex-wrap gap-5">
+              <div className="">Status</div>
+              <div className="">Terms</div>
+              <div className="">Privacy</div>
+              <div className="">OSS</div>
+              <div className="">Sitemap</div>
+              <div className=""> Contact us</div>
+            </div>
+            <div className="">
+              <div className="flex gap-5">
+                <Youtube /> <Twitter /> <Linkedin /> <Github />
+              </div>
+            </div>
+          </div>
+        </div>
+      </MaxWidthContainer>
+    </div>
+  );
 }
